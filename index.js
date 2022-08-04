0ff04295fada5b5eba704f6c80d9187a06901daa
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-03 21:17:17 -0600

    working Tic Tac Toe game

--- index.js
+++ index.js
@@ -23,11 +23,10 @@ app.listen(PORT, () => {
 // added gameBoard.ejs
 
 // This variable will determine what turn it is
-let gamestate = [1, 0, 0, 0, 0, 0, 0, 0, 0];
+let gamestate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Step 6 Add GET and POST requests
 app.get("/play", (req, res) => {
-  res.cookie('game_state', gamestate);
   let templateVars = {
     gameState: gamestate,
     turnNumber: turnNumber(gamestate)
@@ -35,12 +34,105 @@ app.get("/play", (req, res) => {
   res.render("gameBoard", templateVars);
 });
 
+app.get("/reset", (req, res) => {
+  gamestate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  res.redirect('/play');
+});
 
+app.get('/e1', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[0] === 0 && turn === 0) {
+    gamestate[0] = 1;
+  } else if (gamestate[0] === 0 && turn === 1) {
+    gamestate[0] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e2', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[1] === 0 && turn === 0) {
+    gamestate[1] = 1;
+  } else if (gamestate[1] === 0 && turn === 1) {
+    gamestate[1] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e3', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[2] === 0 && turn === 0) {
+    gamestate[2] = 1;
+  } else if (gamestate[2] === 0 && turn === 1) {
+    gamestate[2] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e4', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[3] === 0 && turn === 0) {
+    gamestate[3] = 1;
+  } else if (gamestate[3] === 0 && turn === 1) {
+    gamestate[3] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e5', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[4] === 0 && turn === 0) {
+    gamestate[4] = 1;
+  } else if (gamestate[4] === 0 && turn === 1) {
+    gamestate[4] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e6', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[5] === 0 && turn === 0) {
+    gamestate[5] = 1;
+  } else if (gamestate[5] === 0 && turn === 1) {
+    gamestate[5] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e7', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[6] === 0 && turn === 0) {
+    gamestate[6] = 1;
+  } else if (gamestate[6] === 0 && turn === 1) {
+    gamestate[6] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e8', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[7] === 0 && turn === 0) {
+    gamestate[7] = 1;
+  } else if (gamestate[7] === 0 && turn === 1) {
+    gamestate[7] = 2;
+  }
+  res.redirect("/play");
+});
+
+app.get('/e9', (req, res) => {
+  let turn = turnNumber(gamestate) % 2;
+  if (gamestate[8] === 0 && turn === 0) {
+    gamestate[8] = 1;
+  } else if (gamestate[8] === 0 && turn === 1) {
+    gamestate[8] = 2;
+  }
+  res.redirect("/play");
+});
 
 // Step 7 Add Functions
 let turnNumber = function(gamestate) {
   let numberTurns = 9;
-  for (const numbers in gamestate) {
+  for (const numbers of gamestate) {
     if (numbers === 0) {
       numberTurns --;
     }
