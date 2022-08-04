a1479aaecc63d56e460b01a1d78fafb19bedc4eb
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-03 20:39:16 -0600

    adding logic to buttons

--- index.js
+++ index.js
@@ -20,13 +20,31 @@ app.listen(PORT, () => {
 });
 
 // Step 5 add Views
-app.get("/", (req, res) => {
-  res.render("gameBoard");
-  //res.send('hello');
-});
+// added gameBoard.ejs
+
+// This variable will determine what turn it is
+let gamestate = [1, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Step 6 Add GET and POST requests
+app.get("/play", (req, res) => {
+  res.cookie('game_state', gamestate);
+  let templateVars = {
+    gameState: gamestate,
+    turnNumber: turnNumber(gamestate)
+  };
+  res.render("gameBoard", templateVars);
+});
+
+
 
 // Step 7 Add Functions
-
+let turnNumber = function(gamestate) {
+  let numberTurns = 9;
+  for (const numbers in gamestate) {
+    if (numbers === 0) {
+      numberTurns --;
+    }
+  }
+  return numberTurns;
+};
 // Step 8 Add Mocha and Chai Tests to test functionality
